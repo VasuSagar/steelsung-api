0c64aa425dce420a89efbe4253b5fd48ebb0deb3
Vasu Sagar <vasu.sagar2605@gmail.com>
2023-08-05 21:09:50 +0530

    misc():Added CrossOrigin for balance API

--- src/main/java/com/vasu/steelsungapi/player/infrastructure/adapters/input/rest/BalanceRestAdapter.java
+++ src/main/java/com/vasu/steelsungapi/player/infrastructure/adapters/input/rest/BalanceRestAdapter.java
@@ -4,10 +4,12 @@ import com.vasu.steelsungapi.player.application.ports.input.GetBalanceUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {"http://localhost:4200", "https://steelsung.vasusagar.com"})
 @RestController
 @RequestMapping("/api/v1/balance")
 @RequiredArgsConstructor
