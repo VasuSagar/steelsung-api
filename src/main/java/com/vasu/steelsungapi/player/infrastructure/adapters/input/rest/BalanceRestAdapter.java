818c3b9dc2c24df30688c367d6af8ad91dcfb540
Vasu Sagar <vasu.sagar2605@gmail.com>
2023-08-21 01:57:01 +0530

    fix():Fixed CORS issue for balance endpoint

--- src/main/java/com/vasu/steelsungapi/player/infrastructure/adapters/input/rest/BalanceRestAdapter.java
+++ src/main/java/com/vasu/steelsungapi/player/infrastructure/adapters/input/rest/BalanceRestAdapter.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = {"http://localhost:4200", "https://steelsung.vasusagar.com"})
 @RestController
 @RequestMapping("/api/v1/balance")
 @RequiredArgsConstructor
